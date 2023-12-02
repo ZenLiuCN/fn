8929b9ea05addfa81ed16f6f1fd9a069b39925ae
Zen.Liu <lcz20@163.com>
2023-12-02 22:36:26 +0800

    fix FsSPA

--- handlers.go
+++ handlers.go
@@ -14,16 +14,19 @@ import (
 // @param root: the root path
 // @param index: the index file path relative to root
 func StaticSinglePageApp(root, index string) http.Handler {
+	if root == "" {
+		root = "."
+	}
 	return spa{root, index}
 }
 
 type spa [2]string
 
 func (h spa) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := filepath.Join(h[0], r.URL.Path)
-	if h[0] == "" && path[0] == '/' {
-		path = path[1:]
+	if h[0] == "" {
+		h[0] = "."
 	}
+	path := filepath.Join(h[0], r.URL.Path)
 	fi, err := os.Stat(path)
 	if os.IsNotExist(err) || fi.IsDir() {
 		http.ServeFile(w, r, filepath.Join(h[0], h[1])) // when not found or is dir,redirect to index
@@ -33,6 +36,7 @@ func (h spa) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	http.FileServer(http.Dir(h[0])).ServeHTTP(w, r)
 }
 
@@ -41,6 +45,9 @@ func (h spa) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // @param root: the root folder in fs.FS, can be empty
 // @param index: the index file path in filesystem, panic when not exists
 func FsSinglePageApp(f fs.FS, root, index string) http.Handler {
+	if root == "" {
+		root = "."
+	}
 	return &spaFs{f, root, Panic1(f.Open(filepath.Join(root, index)))}
 }
 
@@ -52,9 +59,6 @@ type spaFs struct {
 
 func (h *spaFs) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := filepath.Join(h.root, r.URL.Path)
-	if h.root == "" && path[0] == '/' {
-		path = path[1:]
-	}
 	fi, err := h.FS.Open(path)
 	if os.IsNotExist(err) || Panic1(fi.Stat()).IsDir() {
 		if h.index == nil {
