f472f8297a7d25110478325883bf9b39e0bad3a1
Zen.Liu <lcz20@163.com>
2023-12-02 19:30:05 +0800

    Signals with default value

--- singals.go
+++ singals.go
@@ -4,10 +4,17 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 // WithSignal run a contextual function with notify by signals, the returning context.CancelFunc is optional to invoke.
 func WithSignal(fn func(ctx context.Context), signals ...os.Signal) context.CancelFunc {
+	if len(signals) == 0 {
+		signals = []os.Signal{
+			syscall.SIGINT,
+			syscall.SIGTERM,
+		}
+	}
 	ctx, cc := signal.NotifyContext(context.Background(), signals...)
 	go func() {
 		fn(ctx)
